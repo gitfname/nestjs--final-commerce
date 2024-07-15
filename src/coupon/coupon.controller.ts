1528e9b9e2814f62995e43ab9c78215deecc39a0
gitfname <newamailnam@gmail.com>
2024-07-16 00:29:19 +0430

    guards has been added to the coupon module

--- src/coupon/coupon.controller.ts
+++ src/coupon/coupon.controller.ts
@@ -1,20 +1,28 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { CouponService } from './coupon.service';
 import { CreateCouponDto } from './dto/create-coupon.dto';
 import { UpdateCouponDto } from './dto/update-coupon.dto';
 import { Coupon } from './entities/coupon.entity';
 import { AddProductToCouponReqParamsDto } from 'src/coupon/dto/add-product-to-coupon-req-params.dto';
 import { DeleteProductFromCouponReqParamsDto } from './dto/delete-product-from-coupon-req-params.dto';
+import { AuthGuard } from 'src/guards/auth.guard';
+import { RoleGuard } from 'src/guards/role.guard';
+import { Roles } from 'src/rbac/roles.decoator';
+import { ERoles } from 'src/rbac/user-roles.enum';
 
 @Controller('coupons')
 export class CouponController {
   constructor(private readonly couponService: CouponService) { }
 
   @Post()
+  @Roles(ERoles.Owner, ERoles.Admin)
+  @UseGuards(AuthGuard, RoleGuard)
   async create(@Body() createCouponDto: CreateCouponDto): Promise<Coupon> {
     return await this.couponService.create(createCouponDto)
   }
 
+  @Roles(ERoles.Owner, ERoles.Admin)
+  @UseGuards(AuthGuard, RoleGuard)
   @Post("add-product/:couponId/:productId")
   async addProductToCoupon(@Param() params: AddProductToCouponReqParamsDto): Promise<void> {
     await this.couponService.addProductToCoupon(+params.productId, +params.couponId)
@@ -42,16 +50,22 @@ export class CouponController {
     return await this.couponService.findOne(+id);
   }
 
+  @Roles(ERoles.Owner, ERoles.Admin)
+  @UseGuards(AuthGuard, RoleGuard)
   @Patch(':id')
   async update(@Param('id') id: string, @Body() updateCouponDto: UpdateCouponDto): Promise<void> {
     await this.couponService.update(+id, updateCouponDto);
   }
 
+  @Roles(ERoles.Owner, ERoles.Admin)
+  @UseGuards(AuthGuard, RoleGuard)
   @Delete("delete-product/:productId")
   async deleteProductFromCoupon(@Param() params: DeleteProductFromCouponReqParamsDto): Promise<void> {
     await this.couponService.deleteProductFromCoupon(+params.productId)
   }
 
+  @Roles(ERoles.Owner, ERoles.Admin)
+  @UseGuards(AuthGuard, RoleGuard)
   @Delete(':id')
   async remove(@Param('id') id: string): Promise<void> {
     await this.couponService.remove(+id);
